b19a51eeccc5ac18d4bc0e9dc9412a8709b91233
imneell <73930342+maffven@users.noreply.github.com>
2022-02-08 00:39:31 +0300

    Bins classes DB done

--- lib/model/BinLevel.dart
+++ lib/model/BinLevel.dart
@@ -1,9 +1,106 @@
-class BinLevel{
+import 'package:flutter_application_1/db/DatabaseHelper.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+final String tableBinLevel = 'BinLevel';
+class BinLevelFields {
 
-final int binID=0;
-final String level = "";
-final String full = "";
-final String half_full = "";
-final String empty = "";
-
+    static final List<String> values = [
+    //add all fields
+    binID, level, full, half_full,empty
+  ];
+  static final String binID = "_BinId";
+  static final String level = "_level";
+  static final String full = "_full";
+  static final String half_full = "_half_full";
+  static final String empty = "_empty";
 }
+
+class BinLevel {
+  final int binID;
+  final String level;
+ final String full;
+ final String half_full;
+ final String empty;
+
+
+  const BinLevel(
+      {@required this.binID,
+      @required this.level,
+      @required this.full,
+      @required this.half_full,
+     @required this.empty,
+      });
+
+  //Convert BinLevel object to json object
+  Map<String, dynamic> toJson() => {
+        BinLevelFields.binID: binID,
+        BinLevelFields.level: level,
+        BinLevelFields.full: full,
+        BinLevelFields.half_full: half_full,
+        BinLevelFields.empty: empty,
+      };
+
+  BinLevel copy(
+          {int binID,
+          String full,
+          String level,
+          String half_full,
+          String empty
+      }) =>
+      BinLevel(
+          binID: binID ?? this.binID,
+          level: level ?? this.level,
+          half_full: half_full ?? this.half_full,
+          empty: empty ?? this.empty,
+          full: full ?? this.full
+          );
+
+
+
+  //convert from json to MunicipalityAdmin
+  static BinLevel fromJson(Map<String, Object> json) =>
+      BinLevel(
+          binID: json[BinLevelFields.binID] as int,
+          level: json[BinLevelFields.level] as String,
+          half_full: json[BinLevelFields.half_full] as String,
+          full: json[BinLevelFields.full] as String,
+          empty: json[BinLevelFields.empty] as String
+          );
+
+  Future<BinLevel> read(int id, dynamic instance) async {
+    final db = await instance.database;
+    final maps = await db.query(
+      tableBinLevel,
+      columns: BinLevelFields.values,
+      where: '${BinLevelFields.binID} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return BinLevel.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not founs');
+    }
+  }
+
+
+  Future<List<dynamic>> readAll(dynamic instance) async {
+    final db = await instance.database;
+    final result = await db.query(tableBinLevel);
+    return result.map((json) => BinLevel.fromJson(json)).toList();
+  }
+
+  Future<int> update(
+      int id, dynamic instance, BinLevel municipalityAdmin) async {
+    final db = await instance.database;
+    //we have to convert from object to json
+    return db.update(tableBinLevel, municipalityAdmin.toJson(),
+        where: '${BinLevelFields.binID} = ?', whereArgs: [id]);
+  }
+
+  //delete a row
+  Future<int> delete(int id, dynamic instance) async {
+    final db = await instance.database;
+    return db.delete(tableBinLevel,
+        where: '${BinLevelFields.binID} = ?', whereArgs: [id]);
+  }}
--- lib/model/BinLocation.dart
+++ lib/model/BinLocation.dart
@@ -1,8 +1,98 @@
-class BinLocation{
-  
-  final int binID=0;
-  final String location="";
-  final String coordinateX ="";
-  final String coordinateY ="";
+import 'package:flutter_application_1/db/DatabaseHelper.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
+final String tableBinLocation = 'BinLocation';
+class BinLocationFields {
+
+    static final List<String> values = [
+    //add all fields
+    binID, location, coordinateX, coordinateY
+  ];
+  static final String binID = "_BinId";
+  static final String location = "_Location";
+  static final String coordinateX = "_CoordinateX";
+  static final String coordinateY = "_CoordinateY";
+}
+
+class BinLocation {
+  final int binID;
+  final String location;
+  final String coordinateX;
+  final String coordinateY;
+
+  const BinLocation(
+      {@required this.binID,
+      @required this.coordinateX,
+      @required this.coordinateY,
+      @required this.location});
+
+  //Convert BinLevel object to json object
+  Map<String, dynamic> toJson() => {
+        BinLocationFields.binID: binID,
+        BinLocationFields.coordinateX: coordinateX,
+        BinLocationFields.coordinateY: coordinateY,
+        BinLocationFields.location: location,
+  
+      };
+
+  BinLocation copy(
+          {int binID,
+          String coordinateX,
+          String coordinateY,
+          String location,
+      }) =>
+      BinLocation(
+          binID: binID ?? this.binID,
+          coordinateX: coordinateX ?? this.coordinateX,
+          coordinateY: coordinateY ?? this.coordinateY,
+          location: location ?? this.location);
+
+
+
+  //convert from json to MunicipalityAdmin
+  static BinLocation fromJson(Map<String, Object> json) =>
+      BinLocation(
+          binID: json[BinLocationFields.binID] as int,
+          coordinateX: json[BinLocationFields.coordinateX] as String,
+          coordinateY: json[BinLocationFields.coordinateY] as String,
+          location: json[BinLocationFields.location] as String);
+
+  Future<BinLocation> read(int id, dynamic instance) async {
+    final db = await instance.database;
+    final maps = await db.query(
+      tableBinLocation,
+      columns: BinLocationFields.values,
+      where: '${BinLocationFields.binID} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return BinLocation.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not founs');
+    }
+  }
+
+
+  Future<List<dynamic>> readAll(dynamic instance) async {
+    final db = await instance.database;
+    final result = await db.query(tableBinLocation);
+    return result.map((json) => BinLocation.fromJson(json)).toList();
+  }
+
+  Future<int> update(
+      int id, dynamic instance, BinLocation municipalityAdmin) async {
+    final db = await instance.database;
+    //we have to convert from object to json
+    return db.update(tableBinLocation, municipalityAdmin.toJson(),
+        where: '${BinLocationFields.binID} = ?', whereArgs: [id]);
+  }
+
+  //delete a row
+  Future<int> delete(int id, dynamic instance) async {
+    final db = await instance.database;
+    return db.delete(tableBinLocation,
+        where: '${BinLocationFields.binID} = ?', whereArgs: [id]);
+  }
 }

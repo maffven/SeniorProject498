59c2dcf4817ad52ffbb02748a4cd2b1313bd753c
imneell <73930342+maffven@users.noreply.github.com>
2022-03-27 00:37:46 +0300

    .

--- lib/Screens/map.dart
+++ lib/Screens/map.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter_application_1/model/BinLevel.dart';
+import 'package:flutter_application_1/model/BinLocation.dart';
 import 'package:flutter_application_1/model/tabs_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -17,9 +18,10 @@ var distance = 0.0;
 String color = "";
 String title = "";
 double colorBin = 0.0;
-List<Marker> markers = [];
+
   List<Marker> markerss =[];
 BinLevel level = BinLevel();
+BinLocation location = BinLocation();
 const CameraPosition INITIAL_CAMERA_POSITION = CameraPosition(
   target: LatLng(
     GMAP_DEFAULT_LATITUDE,
@@ -27,7 +29,18 @@ const CameraPosition INITIAL_CAMERA_POSITION = CameraPosition(
   ),
   zoom: GMAP_DEFAULT_ZOOM,
 );
-
+   //list of markers on the map
+   List<Marker>  markers = [
+      Marker(
+          infoWindow: InfoWindow(title: title),
+          markerId: MarkerId(color),
+          position: LatLng(22.3095, 39.8187),
+        icon: BitmapDescriptor.defaultMarkerWithHue(colorBin),
+        onTap: () {
+          MapUtils.openMap(
+         22.3095, 39.8187);  //to open google map app/direct
+          })
+    ];
 void displayMarker(String titlee, String color, double colorPin) {
   print(colorPin);
   print(color);
@@ -78,7 +91,7 @@ void readDistance() {
       print('manar');
       color = 'Red';
       title = "Full";
-     level = BinLevel(binID: 1, level: 3, full: true, half_full: false , empty: false);
+      level = BinLevel(binID: 1, level: 3, full: true, half_full: false , empty: false);
       colorBin = BitmapDescriptor.hueRed;
       displayMarker(title, color, colorBin);
     } else if (distance > 0.0 && distance < 900.0) {
@@ -96,6 +109,7 @@ void readDistance() {
       colorBin = BitmapDescriptor.hueGreen;
       displayMarker(title, color, colorBin);
     }
+   // location = BinLocation(binID: 1, coordinateX: )
  addObj(level, tableBinLevel);
 //list of markers on the map
     /* markers = [
@@ -203,11 +217,13 @@ class _LoginDemoState extends State<HomeDemo> {
       appBar: AppBar(
         backgroundColor: Color(0xffffDD83),
         title: Text("Map"),
+        
       ),
       body: GoogleMap(
-         markers: Set<Marker>.of(markerss),
+         markers: Set<Marker>.of(markers),
         initialCameraPosition: INITIAL_CAMERA_POSITION,
       ),
+      
     );
   }
 }

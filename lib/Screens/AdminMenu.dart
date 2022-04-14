39a3c6e2544fe1b852aea3565a799737b6af8d78
Rawan <roro1999aa@gmail.com>
2022-04-14 23:13:02 +0300

    Merge branch 'master' of https://github.com/maffven/SeniorProject498

---
+++ lib/Screens/AdminMenu.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_1/Screens/DriverDistrictList.dart';
+import 'package:flutter_application_1/Screens/Notification.dart';
+import 'package:flutter_application_1/Screens/profileScreen.dart';
+import 'package:flutter_application_1/screens/SendComplaint.dart';
+import 'package:flutter_application_1/screens/Login.dart';
+import 'package:flutter_application_1/screens/AdminProfile.dart';
+import 'AdminDashboard.dart';
+import 'package:flutter_application_1/screens/ViewComplaints.dart';
+import 'package:flutter_application_1/Screens/DriverDashboard.dart';
+import 'map.dart';
+
+class AdminMenu extends StatelessWidget {
+  const AdminMenu({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Dashboards',
+            icon: Icon(CupertinoIcons.chart_pie),
+          ),
+          BottomNavigationBarItem(
+            label: 'Map',
+            icon: Icon(CupertinoIcons.map),
+          ),
+          BottomNavigationBarItem(
+            label: 'Notifications',
+            icon: Icon(CupertinoIcons.bell),
+          ),
+          BottomNavigationBarItem(
+            label: 'Complaints',
+            icon: Icon(CupertinoIcons.mail),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(CupertinoIcons.person_circle),
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        switch (index) {
+          case 0:
+            //return BarAndPieChartDashboard();
+            //return PieChartDashboard();
+            return DriverDistrictList();
+            break;
+          case 1:
+            return MapScreen();
+            break;
+          case 2:
+            return Notifications();
+            break;
+          case 3:
+            return ViewComplaints();
+            break;
+          case 4:
+            return AdminProfile();
+            // return Notification();
+            //return AdminProfile();
+            // return DriverSatus();
+            break;
+          default:
+            return Login();
+        }
+      },
+    );
+  }
+}
